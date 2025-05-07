6df13c6e15b4b1fbace765d6d9399283241c5c1a
St3v3n831 <yeiler8310@gmail.com>
2025-05-07 00:45:58 -0600

    Creation of the entire student backend

---
+++ api/Models/Student.cs
@@ -0,0 +1,10 @@
+namespace api.Models{
+  public class Student
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public int courseId { get; set; }
+  }
+}
