fb412be7e1491c75e3b691520ac85ab88064f821
St3v3n831 <yeiler8310@gmail.com>
2025-05-14 11:46:45 -0600

    14/5/2025

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5edd5cb01b315b006efa7508667604e919f48dd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27fe94ed116c25e269727bcb7a871a6b02512ab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
