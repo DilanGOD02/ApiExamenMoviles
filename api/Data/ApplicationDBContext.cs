15ed92f1116ca5087d18f19d095ef536d66f7e5c
DilanGutierrez <138705122+DilanGOD02@users.noreply.github.com>
2025-05-14 14:24:31 -0600

    AddImages

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -7,10 +7,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-  public class ApplicationDBContext : DbContext
-  {
-    public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
 
-    public DbSet<Course> Courses { get; set; }
-  }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
 }
